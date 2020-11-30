a302ece365027446f6ec12391ee7c2054e9c37e8
zerone2 <yicho93@naver.com>
2020-11-30 14:28:20 +0900

    [add - flex sample datas] flex-direction, flex-wrap, etc..

---
+++ src/constants/flexConstants.js
@@ -0,0 +1,45 @@
+export const dataSample1 = {
+  title: 'sample 1',
+  optionKey: 'flex-direction',
+  boxStyle: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+  },
+  options: [
+    { value: 'row', label: 'row' },
+    { value: 'row-reverse', label: 'row-reverse' },
+    { value: 'column', label: 'column' },
+    { value: 'column-reverse', label: 'column-reverse' },
+  ],
+}
+
+export const dataSample2 = {
+  title: 'sample 2',
+  optionKey: 'flex-wrap',
+  boxStyle: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    width: '400px',
+  },
+  options: [
+    { value: 'nowrap', label: 'nowrap' },
+    { value: 'wrap', label: 'wrap' },
+    { value: 'wrap-reverse', label: 'wrap-reverse' },
+  ],
+}
+
+export const dataSample3 = {
+  title: 'sample 3',
+  optionKey: 'justify-content',
+  boxStyle: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+  },
+  options: [
+    { value: 'flex-start', label: 'flex-start' },
+    { value: 'flex-end', label: 'flex-end' },
+    { value: 'center', label: 'center' },
+    { value: 'space-between', label: 'space-between' },
+    { value: 'space-around', label: 'space-around' },
+  ],
+}
